1b258156520ef3abb60d5a80e7e9ec22cd1175e9
arilhr <aurielharlih9213@gmail.com>
2022-05-25 20:48:42 +0700

    add why us section

--- src/components/OurService/OurService.js
+++ src/components/OurService/OurService.js
@@ -4,12 +4,12 @@ import ServiceImage from "../../assets/img/img_service.png";
 
 export const OurService = () => {
   return (
-    <div class="our-services" id="our-services">
-      <div class="container row flex-column gap-5 align-items-center m-auto flex-lg-row">
-        <div class="col w-100">
-          <img class="w-100" src={ServiceImage} alt="service" />
+    <div className="our-services" id="our-services">
+      <div className="container row flex-column gap-5 align-items-center m-auto flex-lg-row">
+        <div className="col w-100">
+          <img className="w-100" src={ServiceImage} alt="service" />
         </div>
-        <div class="col d-flex flex-column gap-2 lgw-50">
+        <div className="col d-flex flex-column gap-2 lgw-50">
           <h2>Best Car Rental for any kind of trip in Gresik!</h2>
           <p>
             Sewa mobil di Gresik bersama Binar Car Rental jaminan harga lebih
@@ -17,12 +17,20 @@ export const OurService = () => {
             pelayanan terbaik untuk perjalanan wisata, bisnis, wedding, meeting,
             dll.
           </p>
-          <ul class="services-list list-unstyled d-flex flex-column gap-2">
-            <li class="service-item">Sewa Mobil Dengan Supir di Bali 12 Jam</li>
-            <li class="service-item">Sewa Mobil Lepas Kunci di Bali 24 Jam</li>
-            <li class="service-item">Sewa Mobil Jangka Panjang Bulanan</li>
-            <li class="service-item">Gratis Antar - Jemput Mobil di Bandara</li>
-            <li class="service-item">Layanan Airport Transfer / Drop In Out</li>
+          <ul className="services-list list-unstyled d-flex flex-column gap-2">
+            <li className="service-item">
+              Sewa Mobil Dengan Supir di Bali 12 Jam
+            </li>
+            <li className="service-item">
+              Sewa Mobil Lepas Kunci di Bali 24 Jam
+            </li>
+            <li className="service-item">Sewa Mobil Jangka Panjang Bulanan</li>
+            <li className="service-item">
+              Gratis Antar - Jemput Mobil di Bandara
+            </li>
+            <li className="service-item">
+              Layanan Airport Transfer / Drop In Out
+            </li>
           </ul>
         </div>
       </div>
--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -3,6 +3,7 @@ import { Container } from "react-bootstrap";
 import { HeroBanner } from "../../components/HeroBanner/HeroBanner";
 import { NavbarDefault } from "../../components/Navbar/NavbarDefault";
 import { OurService } from "../../components/OurService/OurService";
+import { WhyUs } from "../../components/WhyUs/WhyUs";
 
 export const Homepage = () => {
   return (
@@ -11,6 +12,7 @@ export const Homepage = () => {
       <div className="home">
         <HeroBanner />
         <OurService />
+        <WhyUs />
       </div>
     </>
   );
