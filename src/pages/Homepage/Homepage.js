d36301bccee55a9c5d46e48025bccd6dcadf1a8f
arilhr <aurielharlih9213@gmail.com>
2022-05-25 12:15:33 +0700

    add hero banner

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -1,10 +1,15 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import { HeroBanner } from "../../components/HeroBanner/HeroBanner";
 import { NavbarDefault } from "../../components/Navbar/NavbarDefault";
 
 export const Homepage = () => {
   return (
     <>
       <NavbarDefault />
+      <div className="home">
+        <HeroBanner />
+      </div>
     </>
   );
 };
