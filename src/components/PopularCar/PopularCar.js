1dad5cf28f93f7cd8b63ee58bfd3a219e6610a31
arilhr <aurielharlih9213@gmail.com>
2022-05-26 10:04:04 +0700

    add responsive to login register page

--- src/components/PopularCar/PopularCar.js
+++ src/components/PopularCar/PopularCar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Bar, Pie } from "react-chartjs-2";
+import { Bar } from "react-chartjs-2";
 import "chart.js/auto";
 import "./PopularCar.scss";
 import { faker } from "@faker-js/faker";
